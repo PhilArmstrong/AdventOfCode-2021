f8b02ad3772f12dbe6b57ccadab1e934351cc19e
Philip Armstrong <phil@kantaka.co.uk>
2021-12-01 10:02:13 +0000

    Use zip/uncurry for part 1.

--- 01/01.hs
+++ 01/01.hs
@@ -4,9 +4,7 @@ main = do
   input <- getContents
   let depths = (map read $ lines input)::[Int]
   -- count values that are larger than previous value
-  let val1 = foldl' (\ (prev,count) next -> if next > prev
-                                            then (next,count+1)
-                                            else (next,count)) (head depths,0) (tail depths)
+  let val1 = count (uncurry (<)) (zip depths (drop 1 depths))
   -- count sliding window sums that are larger than the previous one. (Window size is 3)
   let val2 = foldl' (\ ((a,b,c), count) next -> if (b+c+next) > (a+b+c)
                                                 then ((b,c,next),count+1)
@@ -16,3 +14,6 @@ main = do
           top3 = (one,two,three)
   print val1
   print val2
+
+count p = foldl' (\a x -> if p x then a+1 else a) 0
+
