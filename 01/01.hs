c405bb67b8d255efc9d63bb1c52dd096b9a01c0d
Philip Armstrong <phil@kantaka.co.uk>
2021-12-03 16:05:34 +0000

    Squeeze Day 01 a bit more.

--- 01/01.hs
+++ 01/01.hs
@@ -4,11 +4,5 @@ main = do
   input <- getContents
   let depths = (map read $ lines input)::[Int]
   -- count values that are larger than previous value / window of 3
-  let vals = map (\i -> count $ zipWith (<) depths (drop i depths)) [1,3]
+  let vals = map (\i -> sum $ map fromEnum $ zipWith (<) depths (drop i depths)) [1,3]
   print vals
-
-count :: (Foldable t1, Num a) => t1 Bool -> a
-count = foldl' (\a x -> if x then a+1 else a) 0
-
-countp :: (Foldable t1, Num a) => (b -> Bool) -> t1 b -> a
-countp p = foldl' (\a x -> if p x then a+1 else a) 0
