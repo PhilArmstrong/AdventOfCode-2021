c36f11855b923f36b9c9f848112f9c47364a3fa9
Philip Armstrong <phil@kantaka.co.uk>
2021-12-03 16:27:11 +0000

    Day 03 - squeeze + beautify

--- 03/03.hs
+++ 03/03.hs
@@ -6,10 +6,9 @@ import Debug.Trace
 main = do
   input <- getContents
   let inputNums = map (map digitToInt) $ lines input
-  let num = length inputNums
-  let (g,e) = ge (countBits inputNums) num
+  let (g,e) = ge (countBits inputNums) (length inputNums)
   print ((g,e),g*e)
-  let o2' = bitstoNum $ o2 inputNums
+  let o2'  = bitstoNum $ o2 inputNums
   let co2' = bitstoNum $ co2 inputNums
   print ((o2',co2'),o2'*co2')
 
@@ -17,7 +16,7 @@ bitstoNum :: (Show a, Ord a, Num a) => [a] -> Int
 bitstoNum ns = bitstoNum' 0 0 (reverse ns)
   where
     bitstoNum' num _ [] = num
-    bitstoNum' num b (i:is) = if i>0 then bitstoNum' (setBit num b) (b+1) is else bitstoNum' num (b+1) is
+    bitstoNum' num b (i:is) = bitstoNum' (if i>0 then setBit num b else num) (b+1) is
 
 countBits :: [[Int]] -> [Int]
 countBits is = map sum $ transpose is
@@ -27,18 +26,20 @@ ge counts l =
   let
     half = div l 2
     g' g e b []     = (g,e)
-    g' g e b (c:cs) = if c>half then g' (setBit g b) e (b+1) cs else g' g (setBit e b) (b+1) cs
+    g' g e b (c:cs) | c>half    = g' (setBit g b) e            (b+1) cs
+                    | otherwise = g' g            (setBit e b) (b+1) cs
   in g' 0 0 0 (reverse counts)
 
 o2 :: [[Int]] -> [Int]
 o2 input =
   let
     life ::Int -> [[Int]] -> [Int]
-    life b is | length is == 1 = head is
-              | otherwise      = life (b+1) (filter (\x -> (x!!b) == val) is)
+    life b is | l == 1    = head is
+              | otherwise = life (b+1) (filter (\x -> (x!!b) == val) is)
       where
-        half = div (length is+1) 2
-        val = if (countBits is)!!b >= half then 1 else 0
+        l = length is
+        half = div (l+1) 2
+        val = if countBits is!!b >= half then 1 else 0
   in
     life 0 input
 
@@ -46,10 +47,11 @@ co2 :: [[Int]] -> [Int]
 co2 input =
   let
     life :: Int -> [[Int]] -> [Int]
-    life b is | length is == 1 = head is
-              | otherwise      = life (b+1) (filter (\x -> (x!!b) == val) is)
+    life b is | l == 1    = head is
+              | otherwise = life (b+1) (filter (\x -> (x!!b) == val) is)
       where
-        half = div (length is+1) 2
-        val = if (countBits is)!!b < half then 1 else 0
+        l = length is
+        half = div (l+1) 2
+        val = if countBits is!!b >= half then 0 else 1
   in
     life 0 input
