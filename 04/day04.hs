6234052de799edd2d7f27e44b0d2dde82defbada
Philip Armstrong <phil@kantaka.co.uk>
2021-12-09 12:26:04 +0000

    Day 04: Part 2

--- 04/day04.hs
+++ 04/day04.hs
@@ -5,6 +5,7 @@ import qualified Data.Vector as V
 import qualified Data.Map.Strict as M
 import Debug.Trace
 import Data.Maybe
+import Data.Either
 --newtype Board = Array (Int,Int)
 type Board = ([[Int]], [[Int]]) -- second is transpose of the first.
 
@@ -16,26 +17,25 @@ main = do
   let boards' = map (map (map read . words) . (filter (/= []) . splitOn "\n")) boards
   let boardst = zip boards' (map transpose boards')
 --  let boardsArray = map (listArray ((0,0),(4,4))) boards
-  print $ solve nums' boardst
+  let result = solve nums' boardst
+  putStrLn $ "Part1: "++show (head result)
+  putStrLn $ "Part2: "++show (last result)
 
-solve :: [Int] -> [Board] -> Int
-solve [] boards = error "No more numbers!"
+solve :: [Int] -> [Board] -> [Int]
+solve [] boards = []
 solve (n:ns) boards = let
-  newboards = traceShowId $ map (filterBoard n) boards
-  checkBoards = traceShowId $ map checkBoard newboards
-  result = catMaybes checkBoards
+  newboards = map (filterBoard n) boards
+  checkBoards = map checkBoard newboards
   in
-    case result of
-      [] -> solve ns newboards
-      x:xs -> traceShow (n,x) $ n*x
+    map (n*) (lefts checkBoards) ++ solve ns (rights checkBoards)
 
-checkBoard :: Board -> Maybe Int
+checkBoard :: Board -> Either Int Board
 checkBoard (x,xt) =
   if elem [] x || elem [] xt
   then
-    Just $ sum (map sum x)
+    Left $ sum (map sum x)
   else
-    Nothing
+    Right (x,xt)
 
 filterBoard :: Int -> Board -> Board
 filterBoard n (x,xt) = (map (filter (/= n)) x, map (filter (/= n)) xt)
